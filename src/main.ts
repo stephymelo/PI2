ec6db0d2625226e809a8dcd3c6bf7add3fefae74
stephym <stephymelo99@gmail.com>
2021-10-29 17:33:19 -0500

    menu

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import './style.css'
 import Register from './pages/Register/Register';
 import Login from './pages/Login/Login';
+import Menu from './components/Menu';
+
 
 const register = new Register();
 const login = new Login();
+const menu = new Menu();
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
-app.innerHTML = false ? register.render() : login.render();
+app.innerHTML = false ? register.render() : login.render() ; menu.render();
