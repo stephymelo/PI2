b72bd523101549ee5bb915cf628135f98cc1f36e
stephym <stephymelo99@gmail.com>
2021-11-08 00:36:21 -0500

    reactstuff

---
+++ src/Componentes/Register/Register.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './Register.scss';
+import { RegisterObj } from './../Types/RegisterObj';
+
+
+
+export type RegisterProp = RegisterObj & {
+
+  
+  
+  }
+
+const Register: React.FC<RegisterProp> = ({ }) => {
+    return (
+        <div></div>
+    );
+}
+
+export default Register;
