d589ffd5757dfbc1cc4b59d0f4c2b2d9253dd4ca
stephym <stephymelo99@gmail.com>
2021-11-08 18:47:15 -0500

    tipos

--- src/Componentes/App/App.tsx
+++ src/Componentes/App/App.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
-import Menu from './../Menu/Menu';
-
 import './App.css';
+import Menu from './../Menu/Menu';
+import {
+  BrowserRouter as Router,
+  Route,
+  HashRouter,
+  Routes,
+} from "react-router-dom";
+
+
 
 function App() {
   return (
+    
     <div className="App">
+      
        <Menu />
+
+
+       <HashRouter>
+         <Routes>
+           <Route path="/miscupones">
+
+           </Route>
+           <Route path="/juegos">
+
+           </Route>
+           <Route path="/descuentos">
+
+           </Route>
+           <Route path="/perfil">
+
+           </Route>
+           
+         </Routes>
+       </HashRouter>
 
     </div>
   );
