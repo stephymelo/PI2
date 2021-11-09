2c8986eb26295a9f0d56732174e74c93eaed734a
stephym <stephymelo99@gmail.com>
2021-11-08 23:37:37 -0500

    cuponcito

--- src/Componentes/Cupon/CuponElem/CuponElem.tsx
+++ src/Componentes/Cupon/CuponElem/CuponElem.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './_cuponElem.scss'
 import colcafeLogo from "../../../Recursos/imagenes/productLogos/pietranLogo.png"
-
+import zigZag from "../../../Recursos/logos/IconosSVG/cuponZigzag.svg"
 
 interface CuponElem {
 
@@ -11,15 +11,19 @@ const CuponElem: React.FC<CuponElem> = ({ }) => {
 
     return (
         <article className='cupon'>
-            <div className='cupon__section--1'>
-                <img className='cupon__img' src={colcafeLogo}></img>
-            </div>
-            <div className='cupon__section--2'>
-                <h2 className="cupon__section--2__titulo">25% en galletas</h2>
-            </div>
-            <div className='cupon__section--3'>
-                <h3 className="cupon__section--3__fecha">Vence en 2 dias</h3>
-                <p>30-10-2021</p>
+            <div className='cupon__section'>
+                <img className='cupon__section--1__zigzag' src={zigZag} />
+                <div className='cupon__section--1'>
+                    <img className='cupon__img' src={colcafeLogo}></img>
+
+                </div>
+                <div className='cupon__section--2'>
+                    <h2 className="cupon__section--2__titulo">25% en galletas</h2>
+                </div>
+                <div className='cupon__section--3'>
+                    <h3 className="cupon__section--3__vence">Vence en 2 dias</h3>
+                    <p className="cupon__section--3__fecha">30-10-2021</p>
+                </div>
             </div>
         </article>
 
