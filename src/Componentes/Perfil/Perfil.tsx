e09db6c7d6fa46922053f1cd4f67710a2bc582eb
NatalyAM11 <60659258+NatalyAM11@users.noreply.github.com>
2021-11-12 21:25:31 -0500

    Perfil listo

--- src/Componentes/Perfil/Perfil.tsx
+++ src/Componentes/Perfil/Perfil.tsx
@@ -2,39 +2,61 @@ import * as React from 'react';
 import { PerfilObj } from "../../Types/PerfilObj";
 import settings from "../../Recursos/logos/IconosSVG/settings.svg";
 import call from "../../Recursos/logos/IconosSVG/call.svg";
-import zigZag from "../../Recursos/logos/IconosSVG/cuponZigzag.svg";
+import zigZag from "../../Recursos/logos/IconosSVG/zigzagPerfil.svg";
 import lock from "../../Recursos/logos/IconosSVG/lock.svg";
 import calender from "../../Recursos/logos/IconosSVG/calender.svg";
 import view from "../../Recursos/logos/IconosSVG/ojito.svg";
 import jorge from "../../Recursos/imagenes/perfil/jorge.png";
+import particula from "../../Recursos/imagenes/perfil/particula.svg";
+import editPic from "../../Recursos/imagenes/perfil/editPicBtn.png";
+import perfil from "../../Recursos/logos/IconosSVG/perfil.svg";
 
-
-export type PerfilProp =   {
+export type PerfilProp = PerfilObj & {
 
 }
 
 const Perfil: React.FC<PerfilProp> = ({ }) => {
-    
+
     return (
 
         <div>
             <header className="profileHeader">
-                <section className="profileHeader__optionsDiv">
-                    <h1 className="titles">Perfil</h1>
-                    <img className="profileHeader__settingsIcon" src={settings} alt="profileHeader"/>
+
+                <section className="profileHeader__background">
+                    <img className="particle particle__1" src={particula}/>
+                    <img className="particle particle__2" src={particula}/>
+                    <img className="particle particle__3" src={particula}/>
+                    <img className="particle particle__4" src={particula}/>
+                    <img className="particle particle__5" src={particula}/>
+                    <img className="particle particle__6" src={particula}/>
+                    <img className="particle particle__7" src={particula}/>
+                    <img className="particle particle__8" src={particula}/>
                 </section>
 
-                <div className="profileHeader__pictureDiv">
-                    <img className="profileHeader__picture" src={jorge} alt="user__icon"/>
-                    <h3 className="subtitles">Jorge Giraldo</h3>
+                <div className="profileHeader__allInfoHeader">
+
+                    <section className="profileHeader__optionsDiv">
+                        <h1 className="titles">Perfil</h1>
+                        <img className="profileHeader__settingsIcon" src={settings} alt="profileHeader" />
+                    </section>
+
+                    <section className="profileHeader__pictureDiv">
+                        <div className="profileHeader__onlyPictureDiv">
+                            <img className="profileHeader__picture" src={jorge} alt="user__icon" />
+                            <button className="profileHeader__editPicBtn">
+                                <img src={editPic}/></button>
+                        </div>
+                        <h3 className="profileHeader__nameUser">Jorge Giraldo</h3>
+                    </section>
                 </div>
             </header>
+
             <img className="profileHeader__picos" src={zigZag} />
 
             <section className="dataUserDiv">
 
                 <section className="dataUser">
-                    <img className="dataUser__icon" src={jorge} alt="user__icon" />
+                    <img className="dataUser__icon" src={perfil} alt="user__icon" />
                     <div className="dataUser__infoDiv">
                         <h2 className="dataUser__title">Nombre y apellido</h2>
                         <h3 className="dataUser__text">Jorge Giraldo</h3>
@@ -42,7 +64,7 @@ const Perfil: React.FC<PerfilProp> = ({ }) => {
                 </section>
 
                 <section className="dataUser">
-                    <img className="dataUser__icon" src={call} alt="user__phone"/>
+                    <img className="dataUser__icon" src={call} alt="user__phone" />
                     <div className="dataUser__infoDiv">
                         <h2 className="dataUser__title">Celular</h2>
                         <h3 className="dataUser__text">301-5267896</h3>
@@ -58,7 +80,7 @@ const Perfil: React.FC<PerfilProp> = ({ }) => {
                 </section>
 
                 <section className="dataUser">
-                    <img className="dataUser__icon" src={lock} alt="user__password"/>
+                    <img className="dataUser__icon" src={lock} alt="user__password" />
                     <div className="dataUser__infoDiv">
                         <h2 className="dataUser__title">Contraseña</h2>
                         <h3 className="dataUser__text">************</h3>
